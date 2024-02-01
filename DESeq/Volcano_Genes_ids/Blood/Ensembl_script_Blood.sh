e0d60cd9fa332fe73463d3be72ad655736f9d343
Federico Silva <federico.silvagutierrez@unifr.ch>
2024-02-01 01:14:42 +0100

    6

---
+++ DESeq/Volcano_Genes_ids/Blood/Ensembl_script_Blood.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# List of Ensembl IDs
+input_file="Blood_Highlighted_gene_ids.txt"
+
+# Output file for storing gene information
+output_file="gene_info_Blood.txt"
+
+# Function to get gene name from Ensembl webpage
+get_gene_name() {
+  gene_id=$1
+  url="https://www.ensembl.org/Mus_musculus/Gene/Summary?db=core;g=${gene_id}"
+
+  # Use curl to fetch the HTML content
+  html_content=$(curl -Ls "$url")
+
+  # Extract the gene name using grep
+  gene_name=$(echo "$html_content" | awk -F 'Gene: ' 'NF > 1{print $2; exit}' | awk '{print $1}')
+
+  # Check if gene_name is not empty
+  if [ -n "$gene_name" ]; then
+    echo "ID: $gene_id Gene: $gene_name" >> "$output_file"
+  else
+    echo "ID: $gene_id No information found" >> "$output_file"
+  fi
+}
+
+# Loop through the list of gene IDs and get gene information
+while IFS= read -r gene_id; do
+  get_gene_name "$gene_id"
+done < "$input_file"
+
+echo "Gene information saved to $output_file"
